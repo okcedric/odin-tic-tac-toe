c5ec1436bbd8fef8d88cfafb285374fc3fd23c3d
pr0pr3 <salut@tchang.fr>
2021-12-30 19:02:08 +0100

    Add onclick events and player alternance

--- app.js
+++ app.js
@@ -13,20 +13,41 @@ const Gameboard = (function(){
         }
 
     }
+    
     function render() {
         for (let i = 0; i < 9; i++) {
-            _grid.querySelector(`[data-id = "${i}"]`).textContent = _gameBoard[i];
+            if (_gameBoard[i]) {
+                let cell = _grid.querySelector(`[data-id = "${i}"]`);
+                cell.textContent = _gameBoard[i];
+                if (_gameBoard[i] == '×'){
+                    cell.classList.add('red');
+                } else {
+                    cell.classList.add('blue');
+                }
+            }
         }
     }
     
-    function getBoard() {
-        return _gameBoard;
+    
+    function events() {
+        let cells = Array.from(_grid.querySelectorAll('.cell'));
+        cells.map(cell => {
+            cell.addEventListener('click', () => {
+                let id = cell.getAttribute('data-id');
+                if (player1.isMyTurn) {
+                    player1.play(id);
+                } else {
+                    player2.play(id)
+                }
+                toggleTurn();
+            })
+        })
     }
-
+    
     function isFree(position) {
         return !_gameBoard[position]
     }
-
+    
     function write(position,sign) {
         _gameBoard[position] = sign;
     }
@@ -34,10 +55,9 @@ const Gameboard = (function(){
     return {
         init,
         render,
-        getBoard,
         isFree,
         write,
-        
+        events
     }
 })();
 
@@ -49,25 +69,31 @@ const Player = (sign, name) => {
         Gameboard.isFree(position) ? Gameboard.write(position, sign): error('Cette place est déjà prise');
         Gameboard.render();
     }
-
+    
+    let isMyTurn = false;
+    
+    
+    
     return {
         play,
         sign,
         name,
-
     }
+}
+
+const toggleTurn = () => {
+    player1.isMyTurn = !(player1.isMyTurn);
+    player2.isMyTurn = !(player2.isMyTurn);
+    
 }
 
 const error = (message) => console.log(`ERROR : ${message}`);
 
-let player1 = Player("□");
+let player1 = Player("×");
 let player2 = Player('○', "okcedric");
-
-
-
 
 Gameboard.init();
 Gameboard.render();
-player1.play(4);
-player2.play(0);
+Gameboard.events();
+player1.isMyTurn = true;
 Gameboard.render();
