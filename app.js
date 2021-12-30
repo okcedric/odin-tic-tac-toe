620bfd849d791708b362bede871be2d9dc8c3b48
pr0pr3 <salut@tchang.fr>
2021-12-30 23:22:02 +0100

    Add check result function

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const Gameboard = (function(){
-    let _gameBoard = [];
+    let _gameBoard = [] ;
     let _grid=document.querySelector('#grid');
     
     
-    function init() {
-        player1.isMyTurn = true;
-        showPlayer(player1);
+    function init(player = player1) {
+        _gameBoard = [];
+        _grid.innerHTML =""
 
-        
         for (let i= 0; i < 9; i++) {
             let cell = document.createElement('div');
             cell.classList.add('cell');
             cell.setAttribute('data-id',i);
+            console.log(cell);
             _gameBoard.push("");
             _grid.appendChild(cell);
         }
-        
+        events();
+        render();
+        _overlay.classList.remove('blue');
+        _overlay.classList.remove('red');
+        _overlay.classList.add('hidden');   
     }
     
     function showPlayer(player) {
         let playerShower = document.querySelector('#player');
-        console.log(playerShower);
         playerShower.innerHTML = player.sign;
         if (player == player1) {
             playerShower.classList.remove('blue');
@@ -32,13 +35,37 @@ const Gameboard = (function(){
         }
     }
 
+    function checkForWinner(){
+        
+        if(_gameBoard[4]){
+            if ((_gameBoard[4] == _gameBoard[0]) && (_gameBoard[4] == _gameBoard[8])) return _gameBoard[4];
+            if ((_gameBoard[4] == _gameBoard[1]) && (_gameBoard[4] == _gameBoard[7])) return _gameBoard[4];
+            if ((_gameBoard[4] == _gameBoard[2]) && (_gameBoard[4] == _gameBoard[6])) return _gameBoard[4];
+            if ((_gameBoard[4] == _gameBoard[3]) && (_gameBoard[4] == _gameBoard[5])) return _gameBoard[4];
+        };
+        if(_gameBoard[0]){
+            if ((_gameBoard[0] == _gameBoard[1]) && (_gameBoard[0] == _gameBoard[2])) return _gameBoard[0];
+            if ((_gameBoard[0] == _gameBoard[3]) && (_gameBoard[0] == _gameBoard[6])) return _gameBoard[0];
+                };
+        if(_gameBoard[8]){
+            if ((_gameBoard[8] == _gameBoard[2]) && (_gameBoard[8] == _gameBoard[5])) return _gameBoard[8];
+                    if ((_gameBoard[8] == _gameBoard[6]) && (_gameBoard[8] == _gameBoard[7])) return _gameBoard[8];
+                };
 
+        
+                return false
+    }
+    function checkForTie(){
+        let cells = Array.from(_grid.querySelectorAll('.cell'));
+        return cells.every((cell)=> !!cell.textContent);
+    }
     
     function render() {
+        console.log('render');
         for (let i = 0; i < 9; i++) {
-            if (_gameBoard[i]) {
-                let cell = _grid.querySelector(`[data-id = "${i}"]`);
-                cell.textContent = _gameBoard[i];
+            let cell = _grid.querySelector(`[data-id = "${i}"]`);
+                console.log(cell)
+                cell.innerText = _gameBoard[i];
                 if (_gameBoard[i] == '□'){
                     cell.classList.add('red');
                 } else {
@@ -46,7 +73,6 @@ const Gameboard = (function(){
                 }
             }
         }
-    }
     
     
     function events() {
@@ -59,8 +85,10 @@ const Gameboard = (function(){
                 } else {
                     player2.play(id)
                 }
-                toggleTurn();
             })
+        });
+        _overlay.addEventListener('click', () => {
+            Gameboard.init();
         })
     }
     
@@ -78,7 +106,9 @@ const Gameboard = (function(){
         isFree,
         write,
         events,
-        showPlayer
+        showPlayer,
+        checkForWinner,
+        checkForTie,
     }
 })();
 
@@ -87,35 +117,60 @@ const Player = (sign, name) => {
     if (!(name)) name = sign;
 
     const play = (position)=> {
-        Gameboard.isFree(position) ? Gameboard.write(position, sign): error('Cette place est déjà prise');
+
+        if (Gameboard.isFree(position)){
+            Gameboard.write(position, sign)
+            toggleTurn();
+        } 
+        
+        error('Cette place est déjà prise');
         Gameboard.render();
+        let winner = Gameboard.checkForWinner()
+        if(winner) {
+            document.querySelector('#winner').textContent = winner + ' wins!';
+            _overlay.classList.remove('hidden');
+            if (winner == "□") {
+                _overlay.classList.add('red');
+            } else {
+                _overlay.classList.add('blue');
+                
+            }   
+        } 
+        
+        let tie = Gameboard.checkForTie();
+        if (tie) {
+            document.querySelector('#winner').textContent = 'It\'s a tie!';
+            _overlay.classList.remove('hidden');
+
+        }
     }
     
     let isMyTurn = false;
-    
-    
-    
+           
     return {
         play,
         sign,
         name,
+        isMyTurn,
     }
 }
 
+
 const toggleTurn = () => {
+    console.log(player1.isMyTurn);
+    console.log(player2.isMyTurn);
     player1.isMyTurn = !(player1.isMyTurn);
     player2.isMyTurn = !(player2.isMyTurn);
     if (player1.isMyTurn) Gameboard.showPlayer(player1);
     if (player2.isMyTurn) Gameboard.showPlayer(player2);
-    
 }
 
 const error = (message) => console.log(`ERROR : ${message}`);
 
 let player1 = Player("□");
 let player2 = Player('○', "okcedric");
+player1.isMyTurn = true;
+Gameboard.showPlayer(player1);
+let _overlay = document.querySelector('#overlay')
 
 Gameboard.init();
-Gameboard.render();
-Gameboard.events();
-Gameboard.render();
